93bba7b1bf3b01bf8d0514303d6c98212fc83dff
StoddardOXC <stoddard@lxnt.info>
2018-09-27 04:53:31 +0300

    test state for base facilities

--- bin/python/aboutpy.py
+++ bin/python/aboutpy.py
@@ -22,7 +22,7 @@
 #
 
 # this lists state objects the module exports
-__states__ = ['AboutPyPy']
+__states__ = ['AboutPyPy', 'BaseFacilityList']
 
 # this gets base classes and stuff
 from api import *
@@ -43,6 +43,39 @@ class AboutPyPy(State):
         btn.onMouseClick(self.btnOk, 1)
         btn.onKeyboardPress(self.btnOk) # no keysym = any key
         self.add(btn)
+
+    def btnOk(self, *args):
+        self.pop()
+
+class BaseFacilityList(State):
+    def __init__(self, parent):
+        st_w = 300
+        btn_w = 60
+        super(BaseFacilityList, self).__init__(parent,
+            w = st_w, h = 180, x = 10, y = 10,
+            popmode = POPUP_BOTH, ui_category = "geoscape", bg = "BACK01.SCR")
+
+        version = "Python " + sys.version.replace(' (', ' \n(' )
+        tl = TextList(self, 280, 140, 20, 20, "text", "saveMenus")
+        tl.set_columns(80, 80, 80)
+        #tl.set_arrow_column(182, ARROW_VERTICAL);
+
+        btn = TextButton(self, btn_w, 20, st_w/2 - btn_w/2, 160, "button", "saveMenus", self.tr("STR_OK"));
+        btn.onMouseClick(self.btnOk, 1)
+        btn.onKeyboardPress(self.btnOk) # no keysym = any key
+        self.add(tl)
+        self.add(btn)
+
+        bases = Game.get_bases(self)
+        if bases is None or len(bases) == 0:
+            log_info("no bases")
+            return
+
+        for base in bases:
+            log_info("{} : {}".format(base['name'], '\n  '.join(f['type'] for f in base['facilities'])))
+            tl.add_row(base['name'], "shit", "crap")
+            for fac in (f['type'] for f in base['facilities']):
+                tl.add_row("", fac, str(fac["maintenance"]))
 
     def btnOk(self, *args):
         self.pop()
